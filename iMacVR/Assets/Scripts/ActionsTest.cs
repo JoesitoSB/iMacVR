4b3458fcaa868a49aa61b1fb3cd1b042747a3dcb
JoeSB <joe.sb.uc@gmail.com>
2019-08-29 15:15:52 -0500

    Added visual controls and added the action test scripts to both

--- iMacVR/Assets/Scripts/ActionsTest.cs
+++ iMacVR/Assets/Scripts/ActionsTest.cs
@@ -8,7 +8,11 @@ public class ActionsTest : MonoBehaviour
     public SteamVR_Input_Sources handType; // 1
     public SteamVR_Action_Boolean teleportAction; // 2
     public SteamVR_Action_Boolean grabAction; // 3
-    
+
+    private GameObject collidingObject; // 1
+    private GameObject objectInHand; // 2
+
+
     // Update is called once per frame
     void Update()
     {
@@ -23,6 +27,17 @@ public class ActionsTest : MonoBehaviour
         }
     }
 
+    private void SetCollidingObject(Collider col)
+    {
+        // 1
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
     public bool GetTeleportDown() // 1
     {
         return teleportAction.GetStateDown(handType);
@@ -32,4 +47,64 @@ public class ActionsTest : MonoBehaviour
     {
         return grabAction.GetState(handType);
     }
+
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        // 1
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // 2
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    // 3
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+        // 1
+        if (GetComponent<FixedJoint>())
+        {
+            // 2
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            // 3
+            objectInHand.GetComponent<Rigidbody>().velocity = controllerPose.GetVelocity();
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = controllerPose.GetAngularVelocity();
+
+        }
+        // 4
+        objectInHand = null;
+    }
+
 }
