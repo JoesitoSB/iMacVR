d51ff907d2b964d255676b9dccce4ece489509e6
Enrique Lara <brlarach@ucq.edu.mx>
2019-09-05 14:50:31 -0500

    Aun no esta terminado el sistema de Snap

---
+++ iMacVR/Assets/Scripts/SnapSystem/DragObjectTest.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObjectTest : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    private float mZOffset;
+
+    public Transform snaptarget;
+
+    private Rigidbody rb;
+    public bool isPlaced;
+
+    private void Update()
+    {
+        if( snaptarget)
+        {
+            transform.position = snaptarget.position;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+            mOffset = gameObject.transform.position - GetMouseWorldPos();
+            isPlaced = false;
+        
+    }
+    public void OnMouseDrag()
+    {
+        //if (!snaptarget)
+        //{
+        //    transform.position = GetMouseWorldPos() + mOffset;
+        //}
+        transform.position = GetMouseWorldPos() + mOffset;
+
+    }
+    public void OnMouseUp()
+    {
+        if(snaptarget)
+        {
+            Debug.Log(transform.position);
+            transform.position = new Vector3(0,0 ,0 );
+            Debug.Log(transform.position);
+                
+            isPlaced = true;
+        }
+    }
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mZCoord += GetDistance();
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+    private float GetDistance()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)//forward
+        {
+            return .2f;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            return -.2f;
+        }
+        else
+        {
+            return 0;
+        }       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "SnapTarget")
+        {
+            snaptarget = gameObject.transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "SnapTarget")
+        {
+            snaptarget = null;
+        }
+    }
+
+
+
+}
