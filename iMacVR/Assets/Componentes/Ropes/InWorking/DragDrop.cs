3c7740944c4de0afc3e1fa4d60ee9f2a8fcb075e
FernanD02 <43126842+FernanD02@users.noreply.github.com>
2019-09-05 21:36:50 -0500

    Se agrego el asset de obi rope, para la simulacion de cables

---
+++ iMacVR/Assets/Componentes/Ropes/InWorking/DragDrop.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Obi;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    private Color mouseOverColor = Color.green;
+    private Color originalColor = Color.red;
+    Rigidbody ThisRigibody;
+    private bool dragging = false;
+    private float distance;
+    public GameObject Other;
+    private ObiRigidbody otherRigidbody;
+    private ObiRigidbody thisobiObiRigidbody;
+    public bool holding;
+
+    private void Start()
+    {
+        ThisRigibody = GetComponent<Rigidbody>();
+        otherRigidbody = Other.GetComponent<ObiRigidbody>();
+        thisobiObiRigidbody = GetComponent<ObiRigidbody>();
+        otherRigidbody.kinematicForParticles = false;
+        thisobiObiRigidbody.kinematicForParticles = false;
+        //ThisRigibody.constraints = RigidbodyConstraints.FreezePosition;
+    }
+    
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = mouseOverColor;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+            holding = true;
+            //ThisRigibody.constraints = RigidbodyConstraints.FreezePositionY;
+            //thisobiObiRigidbody.kinematicForParticles = true;
+            //otherRigidbody.kinematicForParticles = false;
+
+            //ThisRigibody.useGravity = false;
+            //otherRigidbody.constraints = RigidbodyConstraints.None;
+        }
+        else
+        {
+            holding = false;
+            //ThisRigibody.constraints = RigidbodyConstraints.None;
+            //otherRigidbody.kinematicForParticles = false;
+            //thisobiObiRigidbody.kinematicForParticles = false;
+
+            //ThisRigibody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            //otherRigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            ////ThisRigibody.constraints = RigidbodyConstraints.FreezePositionZ;
+            //ThisRigibody.useGravity = true;
+        }
+
+    }
+}
---
+++ iMacVR/Assets/Componentes/Ropes/InWorking/DragDropController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Obi;
+using UnityEngine;
+
+public class DragDropController : MonoBehaviour
+{
+    public GameObject ropeStart;
+    public GameObject ropeEnd;
+
+    private Rigidbody ropeStartRigidbody;
+    private Rigidbody ropeEndRigidbody;
+    private ObiRigidbody ropeStartObiRigidbody;
+    private ObiRigidbody ropeEndObiRigidbody;
+
+    private bool ropeStartHolding;
+    private bool ropeEndHolding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ropeStartObiRigidbody = ropeStart.GetComponent<ObiRigidbody>();
+        ropeEndObiRigidbody = ropeEnd.GetComponent<ObiRigidbody>();
+        ropeEndRigidbody = ropeEnd.GetComponent<Rigidbody>();
+        ropeStartRigidbody = ropeStart.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ropeStartHolding = ropeStart.GetComponent<DragDrop>().holding;
+        ropeEndHolding = ropeEnd.GetComponent<DragDrop>().holding;
+
+        if (ropeStartHolding && !ropeEndHolding)
+        {
+            ropeStartRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            ropeEndRigidbody.constraints = RigidbodyConstraints.None;
+            ropeStartObiRigidbody.kinematicForParticles = true;
+            ropeEndObiRigidbody.kinematicForParticles = false;
+        }
+        else if (!ropeStartHolding && ropeEndHolding)
+        {
+            ropeEndRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            ropeStartRigidbody.constraints = RigidbodyConstraints.None;
+            ropeEndObiRigidbody.kinematicForParticles = true;
+            ropeStartObiRigidbody.kinematicForParticles = false;
+        }
+        else if(ropeStartHolding && ropeEndHolding)
+        {
+            ropeStartRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            ropeEndRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            ropeStartObiRigidbody.kinematicForParticles = true;
+            ropeEndObiRigidbody.kinematicForParticles = true;
+        }
+        else if(!ropeStartHolding && !ropeEndHolding)
+        {
+            ropeStartRigidbody.constraints = RigidbodyConstraints.None;
+            ropeEndRigidbody.constraints = RigidbodyConstraints.None;
+            ropeStartObiRigidbody.kinematicForParticles = false;
+            ropeEndObiRigidbody.kinematicForParticles = false;
+        }
+    }
+}
