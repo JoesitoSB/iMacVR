38cf939793ec9d369191d95f57a66c8b5b05b3cd
Enrique Lara <brlarach@ucq.edu.mx>
2019-11-26 15:41:19 -0600

    Added turn on and turn off functions

---
+++ iMacVR/Assets/Scripts/TurnOnImac.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnImac : MonoBehaviour
+{
+
+    [SerializeField] MeshRenderer mR;
+    [SerializeField] GameObject RetinaOn;
+    [SerializeField] GameObject canvas;
+    public void Start()
+    {
+        TurnOff();
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            TurnOn();
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            TurnOff();
+        }
+    }
+    public void TurnOff()
+    {
+        mR.enabled = false;
+        RetinaOn.SetActive(true);
+        canvas.SetActive(false);
+    }
+    public void TurnOn()
+    {
+        StartCoroutine("TurnOnC");
+    }
+    public IEnumerator TurnOnC()
+    {
+        canvas.SetActive(true);
+        yield return new WaitForSeconds(4);
+        canvas.SetActive(false);
+        RetinaOn.SetActive(true);
+    }
+}
