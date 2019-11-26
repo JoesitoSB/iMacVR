44bafe204fea375cd22f4ad1ea74d434f705cbf9
JoeASB <joe.sb.uc@gmail.com>
2019-11-26 14:00:23 -0600

    Changes in the area to teleport, starting the mac builder manager and also fix in the collision of the motherboard

---
+++ iMacVR/Assets/Scripts/MacBuilderManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enums;
+
+public class MacBuilderManager : MonoBehaviour
+{
+    public static MacBuilderManager instance;
+    [SerializeField]
+    private TypeSnapableObject[] objectsToPlace = new TypeSnapableObject[6];
+    [SerializeField]
+    private Dictionary<TypeSnapableObject, bool> objectsInPlace = new Dictionary<TypeSnapableObject, bool>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        foreach(var obj in objectsToPlace)
+        {
+            objectsInPlace.Add(obj, false);
+        }
+    }
+
+    public void SetObjectPlacedValue(TypeSnapableObject type, bool _isPlaced)
+    {
+        if(objectsInPlace.ContainsKey(type))
+        {
+            objectsInPlace[type] = _isPlaced;
+        }
+    }
+}
