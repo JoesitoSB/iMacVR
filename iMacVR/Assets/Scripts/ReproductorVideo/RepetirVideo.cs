065e0d8392c082219ad34076cb38c1b00e3e5623
Yo030 <seryrak49@gmail.com>
2019-11-28 02:05:07 -0600

    Se Agrego el video y se arreglo unos bugs con el iPad de video

---
+++ iMacVR/Assets/Scripts/ReproductorVideo/RepetirVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class RepetirVideo : MonoBehaviour
+{
+    public Image BotonPlay;
+    public Sprite PlaySP;
+    public VideoPlayer thisvideoplayer;
+    private CambiarTiempoVideo _CambiarTiempoVideo = new CambiarTiempoVideo();
+    private CambiarSprite _CambiarSprite = new CambiarSprite();
+
+    public void _ReiniciarVideo()
+    {
+        _CambiarTiempoVideo.AgregarTiempo(thisvideoplayer, -(float)thisvideoplayer.length, -(float)thisvideoplayer.length);
+        _CambiarSprite.ChangeSprite(BotonPlay, PlaySP);
+    }
+}
